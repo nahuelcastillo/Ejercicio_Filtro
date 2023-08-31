585f1412d345ce5c184fea488a23443dcb6b614d
nahuel.castillo44@gmail,com <nahuel.castillo44@gmail.com>
2023-08-31 00:40:21 -0300

    v2

--- wsinicial/JaP-S10-List-rangeFilter-main/main.js
+++ wsinicial/JaP-S10-List-rangeFilter-main/main.js
@@ -1,32 +1,58 @@
 const URL = "https://fakestoreapi.com/products";
-const minPrice = document.getElementById("rangeFilterCountMin")
-const maxPr
 const filterBtn = document.getElementById("rangeFilterCount");
+const clearBtn = document.getElementById("clearRangeFilter");
 const texarea = document.getElementById("info");
 
+
 document.addEventListener("DOMContentLoaded", () => {
-    fetch(URL) 
+    fetch(URL)
+    .then(response => response.json())
+    .then(data => {
+        let datas = data;
 
-    function Fetch(){
-        fetch(URL)
-        .then (response => response.json());
-    }
-        // Fetch a URL y organizamos la info obtenida del JSON en un listado
-});
+        //Funcion para mostrar los productos
+        function showProducts(array){
+            const content = ""; //se crea una variable apra guardar los datos pasador por el forEach
+            if(array.length > 0){ // se busca que al filtrar los datos se vea almenos 1 sino pasa al else
+                array.forEach(product => {
+                    content += 
+                    `
+                    <div class="list-group-item active"> 
+                    <h4> ${product.title} </h4>
+                      <p> ${product.price} </p>
+                    </div>
+                    `;
+                texarea.innerHTML = content;    
+                });
+            }
+            else{
+               texarea.innerHTML = `<div> No hay productos en ese margen </div>`
+            }
+        }
+    
+        showProducts(datas);
+    
+        // esta parte filtra todos los datos y agrega el evento al boton filtrar
+        filterBtn.addEventListener("click", ()=> {
+            const minPrice = document.getElementById("rangeFilterCountMin").value;
+            const maxPrice = document.getElementById("rangeFilterCountMax").value;
 
-filterBtn.addEventListener("click, () => { FetchProducts() ;
-});
+            let productFilter = datas.filter((product) => product.price >= minPrice && product.price <= maxPrice);
+
+            showProducts(productFilter);
+        })
 
 
+    })
+   
 
+    //elimina lo que se este filtrando y devuelve la lista de todos los productos
+    clearBtn.addEventListener("click", ()=> {
 
+        showProducts(datas);
 
+    })
+    
+    
 
-
-/*
-            <a href="#" class="list-group-item active"> 
-              <h4 class="list-group-item-heading">{data.title} </h4>
-                <p class="list-group-item-text">{data.price} </p>
-              </a>
-
-*/
+})
