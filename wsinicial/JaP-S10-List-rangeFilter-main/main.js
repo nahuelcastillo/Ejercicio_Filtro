422a343e8d44a1c7af99594277a85bf70962ebbe
nahuel.castillo44@gmail,com <nahuel.castillo44@gmail.com>
2023-08-31 00:38:47 -0300

    v1
    Se realiza todo

--- wsinicial/JaP-S10-List-rangeFilter-main/main.js
+++ wsinicial/JaP-S10-List-rangeFilter-main/main.js
@@ -1,5 +1,32 @@
 const URL = "https://fakestoreapi.com/products";
+const minPrice = document.getElementById("rangeFilterCountMin")
+const maxPr
+const filterBtn = document.getElementById("rangeFilterCount");
+const texarea = document.getElementById("info");
 
 document.addEventListener("DOMContentLoaded", () => {
-    // Fetch a URL y organizamos la info obtenida del JSON en un listado
+    fetch(URL) 
+
+    function Fetch(){
+        fetch(URL)
+        .then (response => response.json());
+    }
+        // Fetch a URL y organizamos la info obtenida del JSON en un listado
 });
+
+filterBtn.addEventListener("click, () => { FetchProducts() ;
+});
+
+
+
+
+
+
+
+/*
+            <a href="#" class="list-group-item active"> 
+              <h4 class="list-group-item-heading">{data.title} </h4>
+                <p class="list-group-item-text">{data.price} </p>
+              </a>
+
+*/
